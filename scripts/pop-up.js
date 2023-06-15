a5ab825b2fd894497d8d545b34a0a4f89d67037c
Emma Clarke <emmacodeswell@gmail.com>
2023-06-15 16:47:06 -0400

    Fix indentation

--- scripts/pop-up.js
+++ scripts/pop-up.js
@@ -4,31 +4,31 @@
 // Create a function that does something when the user clicks on the image
 // The function will create pop-up elements when event is triggered and append them to the doc
     
- const openPopUp = ((event) => {
-   const smolImage = event.target
-   const parent = smolImage.parentNode
-   // create wrapper element (darkens screen around pop-up box)
-   const wrapper = document.createElement('div')
-   const content = document.createElement('div')
-   const title = parent.querySelector('h3').cloneNode(true)
-   const price = parent.querySelector('p').cloneNode(true)
-   const bigImage = parent.querySelector('img').cloneNode()
-   // give the wrapper id
-   wrapper.id = 'pop-up'
-   wrapper.addEventListener('click', closePopUp)
-   wrapper.appendChild(content)
-   content.appendChild(title)
-   content.appendChild(bigImage)
-   content.appendChild(price)
-   // append wrapper to body of html
-   document.body.appendChild(wrapper)
-   // make click handler for exit button, gets rid wrapper from doc 
- })
+const openPopUp = ((event) => {
+  const smolImage = event.target
+  const parent = smolImage.parentNode
+  // create wrapper element (darkens screen around pop-up box)
+  const wrapper = document.createElement('div')
+  const content = document.createElement('div')
+  const title = parent.querySelector('h3').cloneNode(true)
+  const price = parent.querySelector('p').cloneNode(true)
+  const bigImage = parent.querySelector('img').cloneNode()
+  // give the wrapper id
+  wrapper.id = 'pop-up'
+  wrapper.addEventListener('click', closePopUp)
+  wrapper.appendChild(content)
+  content.appendChild(title)
+  content.appendChild(bigImage)
+  content.appendChild(price)
+  // append wrapper to body of html
+  document.body.appendChild(wrapper)
+  // make click handler for exit button, gets rid wrapper from doc 
+})
 
- const closePopUp = ((event) => {
-   const wrapper = event.target
-   document.body.removeChild(wrapper)
- })
+const closePopUp = ((event) => {
+  const wrapper = event.target
+  document.body.removeChild(wrapper)
+})
 
 // Finds all images we want to be clickable
 const images = document.querySelectorAll('.plant > img') 
